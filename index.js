0f012f9b50e93901dcdb49cbac4d6f567f173208
Oleksandr <alexzanderzharkov@gmail.com>
2018-03-28 18:22:37 +0300

    123

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
-let url = 'https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&pageSize=20&page=1&apiKey=d626f7167e81454581c8f31730f31a28';
+let url = 'https://newsapi.org/v2/everything?domains=wsj.com,nytimes.com&pageSize=20&page=';
+let urlEnd = '&apiKey=d626f7167e81454581c8f31730f31a28';
 
-let page = 1;
+let page = 2;
 
-fetch(url)
-    .then(res => res.json())
-    .then((out) => {
-        console.log(out.articles);
-        process(out.articles);
-});
+let animations = ["bounce", "pulse", "fadeIn",
+    "fadeInDown", "fadeInLeft", "fadeInRight", "fadeInUp"];
+
+let component;
+
+window.onload = function () {
+
+    $(document).on('scroll', function() {
+        let o = this;
+        let c = component.$refs.foo;
+        $(".post").each(function (index) {
+            if ($(o).scrollTop() >= $(this).position().top) {
+                if (index == c.lenTrigger() - 1 && !c.getTrigger(index)) {
+                    retrieve();
+                }
+                if (!c.getTrigger(index)) {
+                    c.setTrigger(index);
+                    $(this).addClass("animated");
+                    $(this).addClass(animations[Math.floor(Math.random() * animations.length)]);
+                    $(this).css({opacity: '1'});
+                }
+            }
+        });
+    });
+
+    fetch(url + page + urlEnd)
+        .then(res => res.json())
+        .then((out) => {
+            console.log(out.articles);
+            process(out.articles);
+        }).then(function (data) {
+            component.$refs.foo.triggers();
+    });
+};
+
+function retrieve() {
+    page++;
+    let t = url + page + urlEnd;
+    fetch(t)
+        .then(res => res.json())
+        .then((out) => {
+            console.log(out.articles);
+            component.$refs.foo.merge(out.articles);
+        });
+}
+
 
 function process(msg) {
-    let component = new Vue({
+    component = new Vue({
           el: '#parent1',
           data: {
               list: msg,
@@ -18,11 +59,15 @@ function process(msg) {
     });
 }
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
 Vue.component("post", {
     props: ['news'],
-    template: '<div class="card article">' +
+    template: '<div class="card article post">' +
    '                  <div class="card-content">' +
-   '                      <img :src="news.urlToImage" class="author-image" alt="Placeholder image">' +
+   '                      <img :src="news.urlToImage" onerror="this.src = \'https://picsum.photos/1024/768/?image=\' + getRandomInt(1, 1000);" class="author-image" alt="Placeholder image">' +
    '                  </div>' +
    '                  <div class="card-content">' +
    '                      <div class="media">' +
@@ -48,6 +93,34 @@ Vue.component("newsblog", {
     '             <post v-for="item in news" :news="item"></post>' +
     '          </div>' +
     '      </section>' +
-    '  </div>'
+    '  </div>',
+    data: function() {
+        return {
+            triggered: []
+        };
+    },
+    methods: {
+        merge: function (arr2) {
+            for(let i = 0; i < arr2.length; ++i) {
+                this.news.push(arr2[i]);
+                this.triggered.push(false);
+            }
+        },
+        triggers: function () {
+            for(let i = 0; i < this.news.length; ++i) {
+                this.triggered.push(false);
+            }
+        },
+        getTrigger: function (index) {
+            return this.triggered[index];
+        },
+        setTrigger: function (index) {
+            this.triggered[index] = true;
+        },
+        lenTrigger: function () {
+            return this.triggered.length;
+        }
+    }
 });
+
 
